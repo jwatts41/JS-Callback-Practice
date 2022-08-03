e69a8fcfa612a5e38da00a9984d8dd9092fd2354
jwatts41 <wattsjonovan@yahoo.com>
2022-08-03 17:39:03 -0400

    updated code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const character = newImage('assets/green-character/static.gif')
 let direction = null;
 let x = 100;
 let y = 250;
+move(character).to(100, 250)
 
 function moveCharacter(){ 
     if(direction === 'west'){
